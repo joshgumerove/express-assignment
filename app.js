14e15a2c87e7b4ca79c97c7779ee253379a1fdde
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-08 14:13:30 -0500

    create middleware functions

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log("app working");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("another console log");
+  next();
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.listen(3000);
